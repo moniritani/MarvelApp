6bf4bdb486c51d0f4e3b492aecdda402286f105c
Monir Itani <monir_itani@hotmail.com>
2023-09-28 10:07:14 +0300

    Correct API endpoint URL for Retrieving Character Stories

--- app/src/main/java/com/monir/marvelapp/data/api/ApiVariables.kt
+++ app/src/main/java/com/monir/marvelapp/data/api/ApiVariables.kt
@@ -18,7 +18,7 @@ object ApiVariables {
         const val GET_CHARACTERS        = "characters"
         const val GET_CHARACTER         = "characters/{${Parameters.CHARACTER_ID}}"
         const val GET_CHARACTER_COMICS  = "characters/{${Parameters.CHARACTER_ID}}/comics"
-        const val GET_CHARACTER_STORIES = "characters/{${Parameters.CHARACTER_ID}}/events"
+        const val GET_CHARACTER_STORIES = "characters/{${Parameters.CHARACTER_ID}}/stories"
         const val GET_CHARACTER_SERIES  = "characters/{${Parameters.CHARACTER_ID}}/series"
         const val GET_CHARACTER_EVENTS  = "characters/{${Parameters.CHARACTER_ID}}/events"
     }
--- app/src/main/java/com/monir/marvelapp/data/model/Thumbnail.kt
+++ app/src/main/java/com/monir/marvelapp/data/model/Thumbnail.kt
@@ -1,6 +1,8 @@
 package com.monir.marvelapp.data.model
 
+import java.io.Serializable
+
 data class Thumbnail(
     val path: String,
     val extension: String
-)
+) : Serializable
