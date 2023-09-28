3e07b0dc3fffe461e1d49e179a13db20fc2be39e
Monir Itani <monir_itani@hotmail.com>
2023-09-28 09:46:08 +0300

    Handle Invalid Date Formats in Serialization
    - Updated Gson serialization to manage invalid date formats
    - This change allows Gson to set invalid date fields to null to prevent crashing and ensure smooth serialization

--- app/src/main/java/com/monir/marvelapp/utils/GsonHelper.kt
+++ app/src/main/java/com/monir/marvelapp/utils/GsonHelper.kt
@@ -3,16 +3,40 @@ package com.monir.marvelapp.utils
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import java.lang.reflect.Type
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 
 enum class GsonHelper {
     INSTANCE;
 
-    private val gsonCamelCase = Gson()
-    private val gsonSneakCase = GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()
+    private val gsonSneakCase: Gson by lazy {
+        GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .registerTypeAdapter(Date::class.java, DateDeserializer())
+            .create()
+    }
 
     companion object {
-        fun forCamel(): Gson = INSTANCE.gsonCamelCase
         fun forSneak(): Gson = INSTANCE.gsonSneakCase
+    }
 
+    inner class DateDeserializer : JsonDeserializer<Date?> {
+        private val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.US)
+        override fun deserialize(json: JsonElement?,typeOfT: Type?,context: JsonDeserializationContext?): Date? {
+            return try {
+                if (json?.asString != null) dateFormat.parse(json.asString)
+                else null
+            } catch (e: ParseException) {
+                // log the exception and return null
+                null
+            }
+        }
     }
 }
